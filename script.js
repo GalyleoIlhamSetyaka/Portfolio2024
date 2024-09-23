dd75702f3048ef36240ffaf71228215a9d885763
GalyleoIlhamSetyaka <ilhamsetyaka14@gmail.com>
2024-09-23 14:51:51 +0700

    smtpjs

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ function sendEmail() {
     var email = document.getElementById("emailInput").value;
     
     Email.send({
-      SecureToken: "fbd88ae1-9ba1-420e-b649-6a0ef7054bf7", // Dapatkan dari SMTP.js
+      Host: "smtp.elasticemail.com",
+      SecureToken: "fbd88ae1-9ba1-420e-b649-6a0ef7054bf7",
+      Password: "2BCAAE2DA78A7A765298E56E3C93C2EE72A3F07B0F6329B0730F05631E10C94FDFBACB9BC85338295F2C8498B0C574E8",
       To: 'ilhamsetyaka14@email.com',
       From: "ilhamsetyaka14@gmail.com",
       Subject: "New Contact Form Submission",
